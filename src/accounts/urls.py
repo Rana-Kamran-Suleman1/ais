822fb503def4944093e2458f4cb85a77cb41c5b7
Rana Kamran Suleman <60769981+Rana-Kamran-Suleman1@users.noreply.github.com>
2021-03-20 23:46:54 +0500

    Update urls.py

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views as user_views
+from .views import CustomerCreateView
 from .decorators import unauthenticated_user
 
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('register/', user_views.Register, name='signup'),
     path('login/', unauthenticated_user(auth_views.LoginView.as_view(template_name='user/login.html')), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('customer/', CustomerCreateView.as_view(), name='customer'),
 ]
